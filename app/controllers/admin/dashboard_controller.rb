e39a1f20095d10595c496c250d605534be11ff31
doge33 <sandylx33@gmail.com>
2020-10-15 03:51:18 +0000

    display general stats on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
 
   def show
+    @total_products = Product.sum(:quantity)
+    @total_categories = Category.count
+    puts @total_categories
   end
 end
